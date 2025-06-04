2fcca503c56fe5fb17f31d6d9020af615d17acf8
yuta <zokhidjonyuta@gmail.com>
2025-06-04 23:35:12 +0500

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, \
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
@@ -8,5 +9,9 @@ urlpatterns = [
     path('logout/', LogoutViewCustom.as_view(), name='logout'),
     path('password-change/', PasswordChangeView.as_view(), name="password_change"),
     path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('profile/', dashboard, name='user_profile')
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('profile/', dashboard, name='user_profile'),
 ]
